e0539bd3b8b421f37b39c2afd3e827b49b46a225
William Felipe Naranjo <felipe.naranjo@ias.com.co>
2026-02-25 17:30:52 -0500

    se agrega nueva configuración en la infraestructura para agregar un nuevo adaptador

---
+++ infraestructure/driven-adapter/postgres-adapter/src/main/java/co.com.wfnar.ias/config/PostgresAdapterConfig.java
@@ -0,0 +1,16 @@
+package co.com.wfnar.ias.config;
+
+import co.com.wfnar.ias.IOrderRepository;
+import co.com.wfnar.ias.OrderRepositoryAdapter;
+import co.com.wfnar.ias.gateway.OrderRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresAdapterConfig {
+
+    @Bean
+    public OrderRepository orderRepository(IOrderRepository orderRepository) {
+        return new OrderRepositoryAdapter(orderRepository);
+    }
+}
