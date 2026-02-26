71264d59f92da41b27c66cd5062c935e203db452
William Felipe Naranjo <felipe.naranjo@ias.com.co>
2026-02-26 16:53:06 -0500

    se ajusta los config

--- application/app-service/src/main/java/co/com/wfnar/ias/config/UseCaseBeanConfig.java
+++ application/app-service/src/main/java/co/com/wfnar/ias/config/UseCaseBeanConfig.java
@@ -1,5 +1,7 @@
 package co.com.wfnar.ias.config;
 
+import co.com.wfnar.ias.H2OrderRepositoryAdapter;
+import co.com.wfnar.ias.OrderRepositoryAdapter;
 import co.com.wfnar.ias.OrderUseCase;
 import co.com.wfnar.ias.gateway.OrderRepository;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +10,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class UseCaseBeanConfig {
 
-    @Bean
+/*    @Bean
     public OrderUseCase orderUseCase(OrderRepository orderRepository){
         return new OrderUseCase(orderRepository);
+    }*/
+
+/*    @Bean
+    public OrderUseCase H2OrderUseCase(H2OrderRepositoryAdapter h2OrderRepositoryAdapter){
+        return new OrderUseCase(h2OrderRepositoryAdapter);
+    }*/
+
+    @Bean
+    public OrderUseCase postgresOrderUseCase(OrderRepositoryAdapter orderRepositoryAdapter){
+        return new OrderUseCase(orderRepositoryAdapter);
     }
 
 
--- infraestructure/driven-adapter/h2-adapter/src/main/java/co.com.wfnar.ias/config/H2AdapterConfig.java
+++ infraestructure/driven-adapter/h2-adapter/src/main/java/co.com.wfnar.ias/config/H2AdapterConfig.java
@@ -5,7 +5,9 @@ import co.com.wfnar.ias.H2OrderRepositoryAdapter;
 import co.com.wfnar.ias.gateway.OrderRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("h2")
 @Configuration
 public class H2AdapterConfig {
 
@@ -13,4 +15,6 @@ public class H2AdapterConfig {
     public OrderRepository orderRepository(IH2OrderRepository orderRepository) {
         return new H2OrderRepositoryAdapter(orderRepository);
     }
+
+
 }
